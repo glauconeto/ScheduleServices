cbc1d4913fb2c4766178aac90676be257828954f
glauconeto <glauco.neto@tutanota.com>
2024-10-30 21:28:28 -0300

    Resolvido conflito de merge

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,21 +1,3 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from '../components/auth/Login';
-import Register from '../components/auth/Register';
-
-const Routes = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-                {/* Add more routes as needed */}
-            </Switch>
-        </Router>
-    );
-};
-
-export default Routes;
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
